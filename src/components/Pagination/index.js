817d22cae4e00f54864a2ef300271ee90acbe2c9
vineeth123-v <vangapanduvineethkumar11@gmail.com>
2024-09-02 02:25:17 +0000

    commit message

--- src/components/Pagination/index.js
+++ src/components/Pagination/index.js
@@ -44,6 +44,8 @@ class Pagination extends React.Component {
 
   render() {
     const {pageNo} = this.state
+    // const {totalPages} = this.props
+
     return (
       <div className="mb-3 d-flex justify-content-center align-items-center">
         <button type="button" className="control-btn" onClick={this.onPrevPage}>
